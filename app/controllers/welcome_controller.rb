5eb67b5bb83ace8a58cfb1717502688166b47809
maxinlei <maxinlei_public@126.com>
2017-05-04 13:52:43 +0800

    model-migration

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,25 @@
 class WelcomeController < ApplicationController
+  layout "welcome"
+  after_action :intercom_shutdown, only: [:index]
+
   def index
-    flash[:notice] = "你好"
+    @products = Product.all.sort_by{|product| -product.fans.count}
+    @products = @products.first(8)
+    @p1 = @products[0]
+    @p2 = @products[1]
+    @p3 = @products[2]
+    @p4 = @products[3]
+    @p5 = @products[4]
+    @p6 = @products[5]
+    @p7 = @products[6]
+    @p8 = @products[7]
+  end
+
+  def about
+  end
+
+  protected
+  def intercom_shutdown
+    IntercomRails::ShutdownHelper.intercom_shutdown(session, cookies)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,61 @@
 Rails.application.routes.draw do
-  devise_for :users
-  namespace :admin do
-    resources :products
-  end
+  post '/rate' => 'rater#create', :as => 'rate'
+  get '/info' => 'info#index'
+  devise_for :users, :controllers => {
+    :sessions      => "users/sessions",
+    :registrations => "users/registrations",
+    :passwords     => "users/passwords",
+  }
   resources :products do
+    member do
+      post :add_to_cart
+      post :favorite
+      post :unfavorite
+      post :instant_buy
+    end
     collection do
       get :search
     end
+    resources :reviews, only: [:new, :create]
   end
 
-  root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :categories
+    resources :products
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end
+  end
+
+  resources :carts do
+    collection do
+      delete :clean
+      post :checkout
+    end
+  end
+
+  resources :cart_items
+
+  resources :orders do
+    member do
+      post :pay_with_alipay
+      post :pay_with_wechat
+      post :apply_to_cancel
+    end
+  end
+
+  namespace :account do
+    resources :orders
+  end
+
+  resources :favorites
+
+  root "welcome#index"
+
+  get 'about' => 'welcome#about'
 end
