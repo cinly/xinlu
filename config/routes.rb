c2f0ee7fb9558244babeb746785b4c097251c8e4
cinly <cinlyma@gmail.com>
2017-05-27 11:40:11 +0800

    css c-jdstore

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,27 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
+    resources :exams do
+      member do
+          post :a
+          post :a1
+
+          post :b
+          post :b1
+
+          post :c
+          post :c1
+
+          post :d
+          post :d1
+
+          post :e
+          post :e1
+
+          post :f
+          post :f1
+      end
+    end
     resources :categories
     resources :products
     resources :posts
@@ -33,6 +54,19 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  resources :answers do
+  post :ture_answer
+end
+
+resources :answers
+
+resources :exams do
+  collection do
+    post :option
+    post :check_answer
+  end
+end
 
   resources :carts do
     collection do
@@ -56,6 +90,28 @@ Rails.application.routes.draw do
   end
 
   resources :favorites
+
+  resources :choices do
+    member do
+        post :aa
+        post :aa1
+
+        post :bb
+        post :bb1
+
+        post :cc
+        post :cc1
+
+        post :dd
+        post :dd1
+
+        post :ee
+        post :ee1
+
+        post :ff
+        post :ff1
+      end
+  end
 
   root "welcome#index"
 
